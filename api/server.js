4d6d89194db02e8a09d5a7ce4f7a1160c977cfa8
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-10 20:36:47 -0700

    minor changes to requests to achieve proper function

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require('express')
-const db = require('./model')
+const db = require('./users/model')
 // import shortid and add to object of newUser?
 
 const server = express()
@@ -8,52 +8,36 @@ const server = express()
 server.use(express.json())
 
 server.post('/api/users', (req, res) => {
-  // const userData = req.body
+  const userData = req.body
 
-  // db.insert(userData)
-  //   .then(newUser => {
-  //     if (newUser) {
-  //       res.status(201).json(newUser)
-  //     } else if (!req.body.name || !req.body.bio) {
-  //       res.status(400).json({
-  //         message: "Please provide name and bio for the user"
-  //       })
-  //     }
-  //   })
-  //   .catch(err => {
-  //     res.status(500).json({
-  //       message: "There was an error while saving the user to the database"
-  //     })
-  //   })
-
-    const newUser = db.insert({
-      name: req.body.name,
-      bio: req.body.bio
-    })
-
+  db.insert(userData)
+    .then(newUser => {
       if (newUser) {
         res.status(201).json(newUser)
       } else if (!req.body.name || !req.body.bio) {
         res.status(400).json({
           message: "Please provide name and bio for the user"
         })
-      } else {
+      }
+    })
+    .catch(err => {
       res.status(500).json({
         message: "There was an error while saving the user to the database"
       })
-    }
+    })
 })
 
 server.get('/api/users', (req, res) => {
-  const users = db.find()
-
-  if (users) {
-    res.json(users)
-  } else {
-    res.status(500).json({
-      message: "The users information could not be retrieved"
+  
+  db.find()
+    .then(users => {
+      res.json(users)
     })
-  }
+    .catch(err => {
+      res.status(500).json({
+        message: "The users information could not be retrieved"
+      })
+    })
 })
 
 server.get('/api/users/:id', (req, res) => {
@@ -82,7 +66,7 @@ server.delete('/api/users/:id', (req, res) => {
   db.remove(id)
     .then(user => {
       if (user) {
-        res.status(204).json(user)
+        res.status(200).json(user)
       } else {
         res.status(404).json({
           message: "The user with the specified ID does not exist"
@@ -103,7 +87,7 @@ server.put("/api/users/:id", (req, res) => {
   db.findById(id)
     .then(user => {
       if (user) {
-        db.update(changes, id)
+        db.update(id, changes)
           .then(updatedUser => {
             res.status(200).json(updatedUser)
           })
