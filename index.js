7585ec897bd7da415cf7d73327bb62ccc1daec97
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-02-18 18:26:08 -0700

    gave dynamic port value and required dotenv on server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const server = require('./api/server');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // START YOUR SERVER HERE
 server.listen(port, () => {
-  console.log("server listening at port 5000")
+  console.log(`server listening at ${port}`)
 })
